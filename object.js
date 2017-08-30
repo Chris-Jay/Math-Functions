9163cc923103e7a16468bdec81fce78e8b8a45be
Chris-Jay <30878029+Chris-Jay@users.noreply.github.com>
2017-08-30 10:37:10 -1000

    add a javascript file
    we practice using objects and also calling them

---
+++ object.js
@@ -0,0 +1,21 @@
+var chris={
+name:"Guy",
+age:16,
+grade:11,
+friends:["Justin","Matt","Dyson","Russell"]
+};
+var christian={
+name:"Guys",
+age:17,
+grade:12,
+friends:["Justice","Mattw","Bryson","Russ"]
+};
+var christ={
+name:"boi",
+age:18,
+grade:12,
+friends:["Christopher","William","Steve","Noah"]
+};
+var x =[chris,christian,christ];
+
+window.alert(x[1].friends[0]);
